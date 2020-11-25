0166b83dc230752b5c36f315944f746e5da73121
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-11-25 16:21:17 +0800

    Create 112502.QS大学排行榜.c

---
+++ 112502.QS大学排行榜.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+int x;
+scanf("%d",&x);
+if(x==1){
+printf("哈佛大学");}
+else if(x==2){
+printf("麻省理工学院");}
+else if(x==3){
+printf("斯坦福大学");}
+else if(x==4){
+printf("加州大学伯克利分校");}
+else if(x==5){
+printf("牛津大学");}
+else if(x==6){
+printf("加州理工大学");}
+else if(x==7){
+printf("剑桥大学");}
+else if(x==8){
+printf("哥伦比亚大学");}
+else if(x==9){
+printf("普林斯顿大学");}
+else if(x==10){
+printf("约翰霍普金斯大学");}
+else{
+printf("数字靠后，请重新输入");}
+}
