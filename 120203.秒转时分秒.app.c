be5556997dec7cd5611844bfab9a57cc7b1d5e74
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-02 16:42:27 +0800

    Create 120203.秒转时分秒.app.c

---
+++ 120203.秒转时分秒.app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,d;
+	printf("================请输入秒数================\n");
+	scanf("%d",&a);
+	b=a/3600;
+	c=(a%3600)/60;
+	d=a-3600*b-60*c;
+	printf("%d时%d分%d秒",b,c,d);
+}
