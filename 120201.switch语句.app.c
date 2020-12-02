926f2e5d05b1842aff18a4df94526cdce6af19a8
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-02 08:57:44 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main(){
+int x;
+printf("欢迎使用QS全球大学排名小程序\n");
+scanf("%d",&x);
+switch(x){
+case 1:printf("麻省理工学院\n");break;
+case 2:printf("斯坦福大学\n");break;
+case 3:printf("哈佛大学\n");break;
+case 4:printf("剑桥大学\n");break;
+case 5:printf("加利福尼亚理工学院\n");break;
+case 6:printf("牛津大学\n");break;
+case 7:printf("伦敦大学学院\n");break;
+case 8:printf("苏黎世联邦理工学院\n");break;
+case 9:printf("帝国理工学院\n");break;
+case 10:printf("芝加哥大学\n");break;
+case 11:printf("普林斯顿大学\n");break;
+case 12:printf("新加坡国立大学\n");break;
+case 13:printf("南洋理工大学\n");break;
+case 14:printf("洛桑联邦大学\n");break;
+case 15:printf("耶鲁大学\n");break;
+case 16:printf("康奈尔大学\n");break;
+case 17:printf("约翰霍普金斯大学\n");break;
+case 18:printf("宾夕法尼亚大学\n");break;
+case 19:printf("爱丁堡大学\n");break;
+case 20:printf("哥伦比亚大学\n");break;
+default:printf("输入错误！请输入1到20以内的数\n");
+}
+}
