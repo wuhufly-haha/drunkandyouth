0ccd8dd1d631e5c5d843b8ef71754487929c5726
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-03 21:00:29 +0800

    Create 120301.一维数组遍历.app.c

---
+++ 120301.一维数组遍历.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int b;
+	int tang[]={1,2,3};
+	int tangm[2][3]={
+		{4,5,6},
+		{7,8,9}
+  };
+	
+	for(b=0;b<3;b++){
+	  printf("%d",tang[b]);
+	}
+		printf("\n");
+}
