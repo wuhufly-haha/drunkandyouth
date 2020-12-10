87381ea23e2512932281c9773371c94c192cbd7b
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-10 19:33:08 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -3,13 +3,13 @@
 int main(){
 	char x[50];
 	scanf("%s",&x);
-	if(strcmp(x,"春节")){
+	if(!strcmp(x,"春节")){
 		printf("吃饺子\n");}
-	else if(strcmp(x,"端午节")){
+	else if(!strcmp(x,"端午节")){
 		printf("吃粽子\n");}
-	else if(strcmp(x,"中秋节")){
+	else if(!strcmp(x,"中秋节")){
 		printf("吃月饼\n");}
-	else if(strcmp(x,"元宵节")){
+	else if(!strcmp(x,"元宵节")){
 		printf("吃汤圆\n");}
 	else{
 	printf("不是在吃，就是在去吃的路上");}
