59fd3d774ec4f33b6ea86c55d41b93d596e99797
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-10 20:27:52 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -2,18 +2,19 @@
 #include<string.h>
 int main(){
 	char x[50];
+	char a[]="春节";
+	char b[]="端午节";
+	char c[]="中秋节";
+	char d[]="元宵节";
 	scanf("%s",&x);
-	if(!strcmp(x,"春节")){
-		printf("吃饺子\n");}
-	else if(!strcmp(x,"端午节")){
-		printf("吃粽子\n");}
-	else if(!strcmp(x,"中秋节")){
-		printf("吃月饼\n");}
-	else if(!strcmp(x,"元宵节")){
-		printf("吃汤圆\n");}
+	if( strcmp(x,a) == 0){
+		printf("吃饺子");}
+	else if( strcmp(x,b) == 0){
+		printf("吃粽子");}
+	else if( strcmp(x,c) == 0){
+		printf("吃月饼");}
+	else if( strcmp(x,d) == 0){
+		printf("吃汤圆");}
 	else{
 	printf("不是在吃，就是在去吃的路上");}
-
-
-
 }
