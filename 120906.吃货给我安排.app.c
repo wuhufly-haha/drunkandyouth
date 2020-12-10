ff1401d6a82fb396f734c0c38803164ad7613aa5
wuhufly-haha <74295017+wuhufly-haha@users.noreply.github.com>
2020-12-10 19:10:38 +0800

    Update and rename 风风光光 to 120906.吃货给我安排.app.c

---
+++ 120906.吃货给我安排.app.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char x[50];
+	scanf("%s",&x);
+	if(strcmp(x,"春节")){
+		printf("吃饺子\n");}
+	else if(strcmp(x,"端午节")){
+		printf("吃粽子\n");}
+	else if(strcmp(x,"中秋节")){
+		printf("吃月饼\n");}
+	else if(strcmp(x,"元宵节")){
+		printf("吃汤圆\n");}
+	else{
+	printf("不是在吃，就是在去吃的路上");}
+
+
+
+}
